bf6401c16dd299ba3090f27d2c28e7b751e5d5c5
veshu <tcveshu@gmail.com>
2020-10-30 08:19:19 +0545

    support env file

--- config/default.js
+++ config/default.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 module.exports = {
   LOG_LEVEL: process.env.LOG_LEVEL || 'debug',
   PORT: process.env.PORT || 3000,
@@ -5,12 +6,12 @@ module.exports = {
 
   AUTH_SECRET: process.env.AUTH_SECRET || 'mysecret',
   VALID_ISSUERS: process.env.VALID_ISSUERS || '["https://api.topcoder-dev.com", "https://api.topcoder.com", "https://topcoder-dev.auth0.com/", "https://auth.topcoder-dev.com/"]',
-  AUTH0_URL: process.env.AUTH0_URL || 'https://topcoder-dev.auth0.com/oauth/token',
-  AUTH0_AUDIENCE: process.env.AUTH0_AUDIENCE || 'https://u-bahn.topcoder.com',
-  TOKEN_CACHE_TIME: process.env.TOKEN_CACHE_TIME || 90,
-  AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID || 'LEyCiuOrHc7UAFoY0EAAhMulWSX7SrQ5',
-  AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET || 'Q1sQ77w43F5pWsMguK9JsStQwoGNRAD6IN4nyUShzlf24w6-CXD0ubDDT79t28tQ',
-  AUTH0_PROXY_SERVER_URL: process.env.AUTH0_PROXY_SERVER_URL || 'https://auth0proxy.topcoder-dev.com/token',
+  AUTH0_URL: process.env.AUTH0_URL,
+  AUTH0_AUDIENCE: process.env.AUTH0_AUDIENCE,
+  TOKEN_CACHE_TIME: process.env.TOKEN_CACHE_TIME,
+  AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
+  AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET,
+  AUTH0_PROXY_SERVER_URL: process.env.AUTH0_PROXY_SERVER_URL,
 
   TC_API: process.env.TC_API || 'https://api.topcoder-dev.com/v5',
   ORG_ID: process.env.ORG_ID || '36ed815b-3da1-49f1-a043-aaed0a4e81ad',
