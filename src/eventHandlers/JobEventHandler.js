076cc8a40b1b5f9db8aeb30a1e25ddbc30888d75
maxceem <maxceem@gmail.com>
2021-01-06 19:13:59 +0200

    fix: don't cancel ResourceBookings
    ref issue #54

--- src/eventHandlers/JobEventHandler.js
+++ src/eventHandlers/JobEventHandler.js
@@ -7,10 +7,9 @@ const models = require('../models')
 const logger = require('../common/logger')
 const helper = require('../common/helper')
 const JobCandidateService = require('../services/JobCandidateService')
-const ResourceBookingService = require('../services/ResourceBookingService')
 
 /**
- * Cancel all related resource bookings and all related candidates when a job is cancelled.
+ * Cancel all related related candidates when a job is cancelled.
  *
  * @param {Object} payload the event payload
  * @returns {undefined}
@@ -33,7 +32,7 @@ async function cancelJob (payload) {
     return
   }
   // pull data from db instead of directly extract data from the payload
-  // since the payload may not contain all fields when it is from partically update operation.
+  // since the payload may not contain all fields when it is from partially update operation.
   const job = await models.Job.findById(payload.value.id)
   const candidates = await models.JobCandidate.findAll({
     where: {
@@ -44,39 +43,17 @@ async function cancelJob (payload) {
       deletedAt: null
     }
   })
-  const resourceBookings = await models.ResourceBooking.findAll({
-    where: {
-      jobId: job.id,
-      status: {
-        [Op.not]: 'cancelled'
-      },
-      deletedAt: null
-    }
-  })
-  await Promise.all([
-    ...candidates.map(candidate => JobCandidateService.partiallyUpdateJobCandidate(
-      helper.getAuditM2Muser(),
-      candidate.id,
-      { status: 'cancelled' }
-    ).then(result => {
-      logger.info({
-        component: 'JobEventHandler',
-        context: 'cancelJob',
-        message: `id: ${result.id} candidate got cancelled.`
-      })
-    })),
-    ...resourceBookings.map(resource => ResourceBookingService.partiallyUpdateResourceBooking(
-      helper.getAuditM2Muser(),
-      resource.id,
-      { status: 'cancelled' }
-    ).then(result => {
-      logger.info({
-        component: 'JobEventHandler',
-        context: 'cancelJob',
-        message: `id: ${result.id} resource booking got cancelled.`
-      })
-    }))
-  ])
+  await Promise.all(candidates.map(candidate => JobCandidateService.partiallyUpdateJobCandidate(
+    helper.getAuditM2Muser(),
+    candidate.id,
+    { status: 'cancelled' }
+  ).then(result => {
+    logger.info({
+      component: 'JobEventHandler',
+      context: 'cancelJob',
+      message: `id: ${result.id} candidate got cancelled.`
+    })
+  })))
 }
 
 /**
