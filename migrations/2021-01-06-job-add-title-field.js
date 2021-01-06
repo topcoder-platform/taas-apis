28891fdae58c82cd5608853e1fe4a715ae7ecfe8
imcaizheng <imcaizheng@gmail.com>
2021-01-06 21:10:28 +0800

    update the title values of existing jobs in DB

--- migrations/2021-01-06-job-add-title-field.js
+++ migrations/2021-01-06-job-add-title-field.js
@@ -5,7 +5,9 @@
 module.exports = {
   up: queryInterface => {
     return Promise.all([
-      queryInterface.sequelize.query('ALTER TABLE bookings.jobs ADD title VARCHAR(64) NOT NULL')
+      queryInterface.sequelize.query('ALTER TABLE bookings.jobs ADD title VARCHAR(64)'),
+      queryInterface.sequelize.query('UPDATE bookings.jobs SET title=description WHERE title is NULL'),
+      queryInterface.sequelize.query('ALTER TABLE bookings.jobs ALTER COLUMN title SET NOT NULL')
     ])
   },
   down: queryInterface => {
