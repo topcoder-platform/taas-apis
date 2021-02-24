47bb20962363ee330901812c296fd2778fadea49
Maksym Mykhailenko <maxceem@gmail.com>
2021-02-24 12:30:03 +0200

    debug: log M2M token when check if project member
    ref issue #155

--- src/common/helper.js
+++ src/common/helper.js
@@ -602,6 +602,7 @@ function getAuditM2Muser () {
  */
 async function checkIsMemberOfProject (userId, projectId) {
   const m2mToken = await getM2MToken()
+  localLogger.debug({ context: 'checkIsMemberOfProject', message: `m2mToken: ${decodeTom2mToken}` })
   const res = await request
     .get(`${config.TC_API}/projects/${projectId}`)
     .set('Authorization', `Bearer ${m2mToken}`)
