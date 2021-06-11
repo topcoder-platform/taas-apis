ff91b389d6b0ffaa2784d3e1f6d470d086fc7b06
Sushil Shinde <shinde.sushil@gmail.com>
2021-06-11 13:12:53 +0530

    fix: added annunal, another rate type

--- src/services/JobCandidateService.js
+++ src/services/JobCandidateService.js
@@ -203,7 +203,7 @@ fullyUpdateJobCandidate.schema = Joi.object().keys({
     userId: Joi.string().uuid().required(),
     status: Joi.jobCandidateStatus().default('open'),
     externalId: Joi.string().allow(null).default(null),
-    resume: Joi.string().stringAllowEmpty().uri().stringAllowEmpty().allow(null).default(null),
+    resume: Joi.string().stringAllowEmpty().uri().allow(null).default(null),
     remark: Joi.string().stringAllowEmpty().allow(null).default(null)
   }).required()
 }).required()
