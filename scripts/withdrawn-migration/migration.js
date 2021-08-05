fc86113e5d5419aaea342eb736166e56e1bc207d
LieutenantRoger <gair_2015@hotmail.com>
2021-08-05 14:55:42 +0800

    migration  scripts updated

--- scripts/withdrawn-migration/migration.js
+++ scripts/withdrawn-migration/migration.js
@@ -12,19 +12,28 @@ const currentStep = 'Migration'
 async function migration () {
   logger.info({ component: currentStep, message: '*************************** Migration process started ***************************' })
   const filePath = path.join(__dirname, '/temp/')
-  const data = fs.readFileSync(filePath + 'jobCandidate-backup.json', 'utf-8')
-  const jobCandidates = JSON.parse(data)
-  let summary = 0
-  for (var i = 0; i < jobCandidates.length; i++) {
-    const jc = await JobCandidate.findById(jobCandidates[i].id)
-    if (jc) {
-      const oldStatus = jc.status
-      const updated = await jc.update({ status: config.WITHDRAWN_STATUS_CHANGE_MAPPING[jobCandidates[i].status] })
-      summary++
-      logger.info({ component: currentStep, message: `jobCandidate with ${jc.id} status changed from ${oldStatus} to ${updated.status}` })
-    }
-  };
-  logger.info({ component: currentStep, message: `Totally updated ${summary} jobCandidates` })
+  const files = []
+  fs.readdirSync(filePath).forEach(async (file) => {
+    files.push(`${filePath}${file}`)
+  })
+  let totalSum = 0
+  for (let j = 0; j < files.length; j++) {
+    const data = fs.readFileSync(files[j], 'utf-8')
+    const jobCandidates = JSON.parse(data)
+    let summary = 0
+    for (let i = 0; i < jobCandidates.length; i++) {
+      const jc = await JobCandidate.findById(jobCandidates[i].id)
+      if (jc) {
+        const oldStatus = jc.status
+        const updated = await jc.update({ status: config.WITHDRAWN_STATUS_CHANGE_MAPPING[jobCandidates[i].status] })
+        summary++
+        totalSum++
+        logger.info({ component: currentStep, message: `jobCandidate with ${jc.id} status changed from ${oldStatus} to ${updated.status}` })
+      }
+    };
+    logger.info({ component: `${currentStep} Sub`, message: `Updated ${summary} jobCandidates from ${files[j]}` })
+  }
+  logger.info({ component: currentStep, message: `Report: Totally Updated ${totalSum} jobCandidates` })
   logger.info({ component: currentStep, message: '*************************** Migration process finished ***************************' })
 }
 
--- scripts/withdrawn-migration/restore.js
+++ scripts/withdrawn-migration/restore.js
@@ -11,19 +11,28 @@ const currentStep = 'Restore'
 async function restore () {
   logger.info({ component: currentStep, message: '*************************** Restore process started ***************************' })
   const filePath = path.join(__dirname, '/temp/')
-  const data = fs.readFileSync(filePath + 'jobCandidate-backup.json', 'utf-8')
-  const jobCandidates = JSON.parse(data)
-  let summary = 0
-  for (var i = 0; i < jobCandidates.length; i++) {
-    const jc = await JobCandidate.findById(jobCandidates[i].id)
-    if (jc) {
-      const oldStatus = jc.status
-      const updated = await jc.update({ status: jobCandidates[i].status })
-      summary++
-      logger.info({ component: currentStep, message: `jobCandidate with ${jc.id} status restored from ${oldStatus} to ${updated.status}` })
-    }
-  };
-  logger.info({ component: currentStep, message: `Totally restored ${summary} jobCandidates` })
+  const files = []
+  fs.readdirSync(filePath).forEach(async (file) => {
+    files.push(`${filePath}${file}`)
+  })
+  let totalSum = 0
+  for (let j = 0; j < files.length; j++) {
+    const data = fs.readFileSync(files[j], 'utf-8')
+    const jobCandidates = JSON.parse(data)
+    let summary = 0
+    for (var i = 0; i < jobCandidates.length; i++) {
+      const jc = await JobCandidate.findById(jobCandidates[i].id)
+      if (jc) {
+        const oldStatus = jc.status
+        const updated = await jc.update({ status: jobCandidates[i].status })
+        summary++
+        totalSum++
+        logger.info({ component: currentStep, message: `jobCandidate with ${jc.id} status restored from ${oldStatus} to ${updated.status}` })
+      }
+    };
+    logger.info({ component: `${currentStep} Sub`, message: `Restored ${summary} jobCandidates from ${files[j]}` })
+  }
+  logger.info({ component: currentStep, message: `Report: Totally restored ${totalSum} jobCandidates` })
   logger.info({ component: currentStep, message: '*************************** Restore process finished ***************************' })
 }
 
