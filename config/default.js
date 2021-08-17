7b487b1c06357ff217ab8cf4c4a010119ed68aaa
Vikas Agarwal <vikas.agarwal@topcoder.com>
2021-08-17 11:37:09 +0530

    Fixed topcoder taxonomy id

--- config/default.js
+++ config/default.js
@@ -38,7 +38,7 @@ module.exports = {
   // the organization id
   ORG_ID: process.env.ORG_ID || '36ed815b-3da1-49f1-a043-aaed0a4e81ad',
   // the referenced taxonomy id
-  TOPCODER_TAXONOMY_ID: process.env.TOPCODER_TAXONOMY_ID || '7637ae1a-3b7c-44eb-a5ed-10ea02f1885d',
+  TOPCODER_TAXONOMY_ID: process.env.TOPCODER_TAXONOMY_ID || '9cc0795a-6e12-4c84-9744-15858dba1861',
 
   TOPCODER_USERS_API: process.env.TOPCODER_USERS_API || 'https://api.topcoder-dev.com/v3/users',
   // the api to find topcoder members
