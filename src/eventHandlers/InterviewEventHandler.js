9a94fbfeb51288809ad9fa305128142cc8b98042
Maksym Mykhailenko <maxceem@gmail.com>
2021-10-29 01:28:28 +0300

    interview dates timezone format as string

--- src/eventHandlers/InterviewEventHandler.js
+++ src/eventHandlers/InterviewEventHandler.js
@@ -104,7 +104,7 @@ async function sendInterviewScheduledNotifications (payload) {
   try {
     const template = 'taas.notification.interview-link-for-host'
 
-    const TIME_FORMAT = 'dddd MMM. Do, hh:mm a Z'
+    const TIME_FORMAT = 'dddd MMM. Do, hh:mm a z'
 
     const interviewEntity = await Interview.findOne({
       where: {
