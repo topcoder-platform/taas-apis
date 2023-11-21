8945ce7c6f4c1598683ab3caa9a023bf8df39427
Jaouad JAGHRIR <jaghrir.jaouad@gmail.com>
2023-11-21 17:49:28 +0100

    script for generating tcUserId for existing candidates

---
+++ Remove_Ubahn-API_dependency.md
@@ -0,0 +1,26 @@
+# Objectives:
+This document describes the procedure to follow to remove Ubahn-API dependency.
+Currently, the user UUIDs in ubahn-api are used to identify the job candidates in TaaS database `job_candidates.userId`
+
+# TaaS-API updates:
+The updates performed on taas-apis to remove the dependency on Ubahn-API are the following:
+
+- Update `job_candidates` table to have an additional column `tc_user_id` which will hold the Topcoder legacy user id
+- Update `JobCandidate` to have a field `tcUserId` which is mapped to `job_candidates.tc_user_id` column
+- Update POST/PATCH/PUT/GET endpoints related to Job candidates to use the new field `tcUserId` instead of Ubahn `userId`
+
+
+# Service Update procedure:
+
+1. When the updated `taas-apis` code is deployed, run the following command in the taas-apis root folder: `npm run job-candidate:sync`
+   This will synchronize the `JobCandidate` model code updates with the database (i.e. add the new `tc_user_id` column)
+2. [tc-taas-rcrm-data-sync-api](https://github.com/topcoder-platform/tc-taas-rcrm-data-sync-api) updates needs to be pushed in the same time
+   with the taas-apis to minimize losing the updates.
+   There may be some data loss in the following scenario:
+   - `taas-apis` updates deployment is complete, and `tc-taas-rcrm-data-sync-api` updates deployment is not completed yet
+   - A job candidate update occurs in RCRM at this time, `tc-taas-rcrm-data-sync-api` will call `taas-apis` with the
+     old payload data using Ubahn-API user UUID, this will generate an error in `tc-taas-rcrm-data-sync-api`
+     Manual checks of the `tc-taas-rcrm-data-sync-api`/`taas-apis` are needed right after deployment to fix any data loss in the deployment window.
+   - This scenario is unlikely to happen though, there are not too many Job applications, only a few
+   - For example, in the last 30 minutes, there are only 2 job applications updates: ![JobCandiatesUpdates](docs/tc-taas-rcrm-data-sync-api-logInsights.png)
+3. Run the script to generate `tcUserId` for existing job candidates: `npm run job-candidate:tcUserId:generate`
---
+++ scripts/job-candidate/generate-tcUserId-for-existing-candidates.js
@@ -0,0 +1,190 @@
+const Sequelize = require('sequelize')
+const _ = require('lodash')
+const models = require('../../src/models')
+const helper = require('../../src/common/helper')
+const logger = require('../../src/common/logger')
+const config = require('config')
+
+// The number of job candidates to process in a single batch
+const JOB_CANDIDATES_BATCH_SIZE = Number(process.env.JOB_CANDIDATES_BATCH_SIZE) || 200
+
+// The number of milliseconds to wait before proceeding to the next candidates batch (defaults to 1 second)
+const BATCH_WAIT_TIME_MILLISECONDS = Number(process.env.BATCH_WAIT_TIME_MILLISECONDS) || 1000
+
+/**
+ * This script will populate the tc_user_id field for all the existing job candidates
+ * This is a one time use script which loads the users data from u-bahn database and
+ * populates the tc_user_id based on ubahn user UUID
+ *
+ * In addition to the taas-apis configuration parameters, this script requires the following environment variable:
+ * - UBAHN_DB_URL: The ubahn database URL
+ */
+const generateTcUserIdForExistingCandidates = async () => {
+  const dbUrl = process.env.UBAHN_DB_URL
+  if (_.isUndefined(dbUrl)) {
+    console.log('UBAHN_DB_URL environment variable is required to be set. Exiting!!')
+    return
+  }
+
+  // Get the list of job candidates from TaaS database where ubahn user UUID is not null
+  const jobCandidates = await models.JobCandidate.findAll({
+    attributes: ['id', 'userId'], // userId refers to UUID in Ubahn
+    where: {
+      userId: {
+        [Sequelize.Op.ne]: null
+      }
+    },
+    distinct: true
+  })
+
+  // Get the mapping between the Ubahn user UUID and user handle
+  const ubahnDbConnection = await getUbahnDatabaseConnection(dbUrl)
+  const ubahnUUIDToHandleMap = await getUserUbahnUUIDToHandleMap(ubahnDbConnection, jobCandidates)
+  await ubahnDbConnection.close()
+
+  // split the candidates to update in chunks
+  const candidatesBatches = _.chunk(jobCandidates, JOB_CANDIDATES_BATCH_SIZE)
+
+  // process job candidate batches in sequence
+  for (const candidatesBatch of candidatesBatches) {
+    // Prepare the data for updating the candidates in the current batch in parallel
+    const candidatesToUpdateInTaasDbPromises = _.map(candidatesBatch, async candidate => {
+      if (!_.isUndefined(ubahnUUIDToHandleMap[candidate.userId])) {
+        // Get the member details from member-api by handle
+        const [memberDetails] = await helper.getMemberDetailsByHandles([ubahnUUIDToHandleMap[candidate.userId]])
+        if (!memberDetails) {
+          logger.info(`member details for handle ${ubahnUUIDToHandleMap[candidate.userId]} does not exist`)
+          return null
+        } else {
+          return {
+            id: candidate.id,
+            tcUserId: memberDetails.userId // This is the Topcoder legacy userId (Number)
+          }
+        }
+      } else {
+        logger.info(`handle for user UUID ${candidate.userId} does not exist`)
+        return null
+      }
+    })
+
+    // wait for all promises to resolve
+    let candidatesToUpdateInTaasDB = await Promise.all(candidatesToUpdateInTaasDbPromises)
+
+    // remove null values (i.e where member details does not exist)
+    candidatesToUpdateInTaasDB = _.filter(candidatesToUpdateInTaasDB, c => !_.isNull(c))
+
+    // Generate SQL statements to update tc_user_id for the current users batch in Postgres
+    // bulkUpdate is not supported by sequelize, we perform the update using SQL
+    let updateBatchTcUserIdsSQL = ''
+    for (const candidateToUpdate of candidatesToUpdateInTaasDB) {
+      updateBatchTcUserIdsSQL +=
+        `UPDATE bookings.job_candidates SET tc_user_id=${candidateToUpdate.tcUserId} WHERE id = '${candidateToUpdate.id}';`
+    }
+
+    // Run the query to update job_candidates.tc_user_id in TaaS PostgreSQL DB for all the users in current batch
+    await models.sequelize.query(updateBatchTcUserIdsSQL)
+
+    // update the candidates in Elasticsearch
+    await bulkUpdateJobCandidatesInElasticsearch(candidatesToUpdateInTaasDB)
+
+    // wait for some time before proceeding to next batch
+    await new Promise((resolve) => setTimeout(resolve, BATCH_WAIT_TIME_MILLISECONDS))
+  }
+}
+
+/**
+ * This function gets the mapping between UBAHN user ids and Topcoder handles
+ * The return value is a Promise of a Map between user ubahn ids and Topcoder handles:
+ * {
+ *   "a55fe1bc-1754-45fa-9adc-cf3d6d7c377a": "pshah_manager",
+ *   "95e7970f-12b4-43b7-ab35-38c34bf033c7": "testfordevemail"
+ * }
+ * @param connection - The Ubahn database connection object
+ * @param jobCandidates - The list of job candidates objects we are interested in, each object should have `userId` field
+ *                        Which represents the user UUID in UBAHN
+ */
+const getUserUbahnUUIDToHandleMap = async (connection, jobCandidates) => {
+  // filter by unique not null userIds
+  const uniqUserIds = _.filter(_.uniqBy(jobCandidates, c => c.userId), c => !_.isNull(c.userId))
+
+  const commaSeparatedUbahnUUIDs = _.join(_.map(uniqUserIds, u => `'${u.userId}'`), ',')
+
+  const query = `SELECT id, handle from "Users" where id in (${commaSeparatedUbahnUUIDs})`
+  const userUUIDsWithHandles = await connection.query(query, { type: Sequelize.QueryTypes.SELECT })
+
+  const ubahnIdToHandleMap = {}
+  for (const userUbahnDetails of userUUIDsWithHandles) {
+    ubahnIdToHandleMap[userUbahnDetails.id] = userUbahnDetails.handle
+  }
+
+  return ubahnIdToHandleMap
+}
+
+/**
+ * Gets the connection object to the Ubahn PostgreSQL Database
+ * @param url - The Ubahn PostgreSQL database url
+ */
+const getUbahnDatabaseConnection = async (url) => {
+  return new Sequelize(url)
+}
+
+/**
+ * This function updates the list of given job candidates in bulk in Elasticsearch
+ *
+ * @param candidates - The array of job candidates to update, example of input parameter is:
+ *        id is the id of job candidate record in TaaS DB / Elasticsearch, not to be mixed with ubahn uuid
+ * [
+ *   {
+ *     "id": "827ee401-df04-42e1-abbe-7b97ce7937ff",
+ *     "tcUserId": 88774631
+ *   },
+ *   {
+ *     "id": "a4ea7bcf-5b99-4381-b99c-a9bd05d83a36",
+ *     "tcUserId": 88774631
+ *   },
+ *   {
+ *     "id": "b0fc417b-3f41-4c06-9f2b-8e680c3a03c6",
+ *     "tcUserId": 40152856
+ *   }
+ * ]
+ */
+const bulkUpdateJobCandidatesInElasticsearch = async (candidates) => {
+  const esClient = await helper.getESClient()
+  const body = []
+
+  for (const candidate of candidates) {
+    body.push({
+      update: {
+        _id: candidate.id,
+        _index: config.esConfig.ES_INDEX_JOB_CANDIDATE
+      }
+    })
+
+    body.push({
+      doc: { ...candidate }
+    })
+  }
+
+  try {
+    logger.debug(`Bulk update job candidates in ES - body =${JSON.stringify(body)}`)
+    const result = await esClient.bulk({
+      refresh: false,
+      body
+    })
+    logger.debug(`Result of job candidates bulk updates = ${JSON.stringify(result)}`)
+    if (result.errors) {
+      logger.info(`Job candidates bulk update in ES has errors - result = ${JSON.stringify(result)}`)
+    }
+  } catch (err) {
+    logger.error('Error when bulk updating job candidates in ES')
+    logger.error(err)
+  }
+}
+
+generateTcUserIdForExistingCandidates().then(res => {
+  console.log('tc_user_id is successfully populated for all job candidates in TaaS database')
+  process.exit(0)
+}).catch(err => {
+  console.log('An error occurred when populating the tc_user_id fields for job candidates')
+  console.log(err)
+})
