1384bee55d3bcd5a4e8dd26e5c3c05e2bda2b590
Kiril Kartunov <kiril.kartunov@gmail.com>
2024-01-23 17:52:48 +0200

    improved scripts and sql file for all

---
+++ scripts/alter-bookings-db-tcUserId.sql
@@ -0,0 +1,42 @@
+ALTER TABLE bookings.interviews
+ALTER COLUMN created_by TYPE TEXT,
+ALTER COLUMN updated_by TYPE TEXT;
+
+ALTER TABLE bookings.jobs
+ALTER COLUMN created_by TYPE TEXT,
+ALTER COLUMN updated_by TYPE TEXT;
+
+ALTER TABLE bookings.job_candidates
+ALTER COLUMN created_by TYPE TEXT,
+ALTER COLUMN updated_by TYPE TEXT,
+ADD COLUMN tc_user_id INTEGER;
+
+ALTER TABLE bookings.resource_bookings
+ALTER COLUMN created_by TYPE TEXT,
+ALTER COLUMN updated_by TYPE TEXT,
+ADD COLUMN tc_user_id INTEGER;
+
+ALTER TABLE bookings.roles
+ALTER COLUMN created_by TYPE TEXT,
+ALTER COLUMN updated_by TYPE TEXT;
+
+ALTER TABLE bookings.role_search_requests
+ALTER COLUMN member_id TYPE TEXT,
+ALTER COLUMN created_by TYPE TEXT,
+ALTER COLUMN updated_by TYPE TEXT;
+
+ALTER TABLE bookings.roles
+ALTER COLUMN created_by TYPE TEXT,
+ALTER COLUMN updated_by TYPE TEXT;
+
+ALTER TABLE bookings.user_meeting_settings
+ALTER COLUMN created_by TYPE TEXT,
+ALTER COLUMN updated_by TYPE TEXT;
+
+ALTER TABLE bookings.work_periods
+ALTER COLUMN created_by TYPE TEXT,
+ALTER COLUMN updated_by TYPE TEXT;
+
+ALTER TABLE bookings.work_period_payments
+ALTER COLUMN created_by TYPE TEXT,
+ALTER COLUMN updated_by TYPE TEXT;
